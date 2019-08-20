2b025e028e5de00dfe9f09a49464b78d7bfad19d
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-08-20 08:57:00 +0700

    hoàn thiện giao diện

---
+++ bai6quanlysieuthi/ChiTiet_HD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai6quanlysieuthi
+{
+    public partial class ChiTiet_HD : Form
+    {
+        public ChiTiet_HD()
+        {
+            InitializeComponent();
+        }
+    }
+}
