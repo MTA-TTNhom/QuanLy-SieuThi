681d1dfe32d0dce311bde92aa0ff1dba42f9ef90
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-08-26 23:31:17 +0700

    update ChiTiet_HD

--- bai6quanlysieuthi/ChiTiet_HD.cs
+++ bai6quanlysieuthi/ChiTiet_HD.cs
@@ -61,5 +61,51 @@ namespace bai6quanlysieuthi
                 return;
             }
         }
+
+        private void btnUpdate_CTHD_Click(object sender, EventArgs e)
+        {
+            if (txtMaHoaDon.Text == "" || cbMaMatHang.Text == "" || txtSoLuong.Text == "")
+            {
+                if (txtMaHoaDon.Text == "")
+                    errorProvider1.SetError(txtMaHoaDon, "Chưa có dữ liệu");
+                if (cbMaMatHang.Text == "")
+                    errorProvider1.SetError(cbMaMatHang, "Chưa có dữ liệu");
+                if (txtSoLuong.Text == "")
+                    errorProvider1.SetError(txtSoLuong, "Chưa có dữ liệu");
+            }
+            else
+            {
+                errorProvider1.Clear();
+            }
+            try
+            {
+                int stt = (int)Convert.ToInt32(txtSTT.Text);
+                string mahd = txtMaHoaDon.Text;
+                string mamh = cbMaMatHang.Text;
+                int sl = (int)Convert.ToInt32(txtSoLuong.Text);
+                if (txtMaHoaDon.Text == "" || cbMaMatHang.Text == "" || txtSoLuong.Text == "")
+                {
+                    MessageBox.Show("Phải điền thông tin đầy đủ");
+                    return;
+                }
+                if (CtHoaDonController.Instance.UpdateCTHD(stt, mahd, mamh, sl))
+                {
+                    if (MessageBox.Show("Bạn có muốn sửa hay không", "Sửa", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                    {
+                        MessageBox.Show("Sửa thành công");
+                        ViewCtHoaDon();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Không thành công!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Lôi thêm dữ liệu");
+                return;
+            }
+        }
     }
 }
