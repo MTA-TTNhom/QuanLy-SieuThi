f73881025c296fb378f1b9ce901724cc289ff06c
Khoabdattt52 <phambangbang1235@gmail.com>
2019-08-27 11:07:16 +0700

    code form đăng nhập

--- bai6quanlysieuthi/DangNhap.cs
+++ bai6quanlysieuthi/DangNhap.cs
@@ -16,5 +16,39 @@ namespace bai6quanlysieuthi
         {
             InitializeComponent();
         }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            string username = txtTaiKhoan.Text;
+            string password = txtMatKhau.Text;
+            if (true)
+            {
+                TrangChu f = new TrangChu();
+                this.Hide();
+                f.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Sai tên khoài khoản hoặc mật khẩu!");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                txtMatKhau.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtMatKhau.UseSystemPasswordChar = true;
+            }
+        }
     }
 }
