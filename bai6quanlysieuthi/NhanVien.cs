0e11ec943da468666f5fbd3a98229b72c7bacc6e
Lê Ngọc Dự <55566554+hoten1701@users.noreply.github.com>
2019-09-02 20:42:19 +0700

    add fNV

--- bai6quanlysieuthi/NhanVien.cs
+++ bai6quanlysieuthi/NhanVien.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace bai6quanlysieuthi
 {
-    public partial class NhanVien : Form
+    public partial class NhanVien: Form
     {
         public NhanVien()
         {
