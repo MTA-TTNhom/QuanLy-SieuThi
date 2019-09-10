e6151bac43eeab38cfef39667bfce2947f4e54ea
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-10 21:21:24 +0700

    Khoi tao du kieu trong Model

---
+++ bai6quanlysieuthi/Models/chitietphieuxuat.cs
@@ -0,0 +1,30 @@
+﻿namespace bai6quanlysieuthi.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
+    using System.Data.Entity.Spatial;
+
+
+    [Table("chitietphieuxuat")]
+    public partial class chitietphieuxuat
+    {
+        [Key]
+        public int stt { get; set; }
+
+        [StringLength(20)]
+        public string maxuat { get; set; }
+
+        [StringLength(20)]
+        public string mamathang { get; set; }
+
+        public int soluong { get; set; }
+
+        public float dongia { get; set; }
+
+        
+
+    }
+}
