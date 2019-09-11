b43cb8f6950f5abcc1d51881cf6f6f8ef93f4473
Khoabdattt52 <phambangbang1235@gmail.com>
2019-09-11 09:42:26 +0700

    update nhanvien.cs

--- bai6quanlysieuthi/NhanVien.cs
+++ bai6quanlysieuthi/NhanVien.cs
@@ -53,16 +53,41 @@ namespace bai6quanlysieuthi.Views
 #region View Nhân viên
         private void btnView_NV_Click(object sender, EventArgs e)
         {
-          
+            errorProvider1.Clear();
+            ViewNhanVien();
+
         }
         void ViewNhanVien()
         {
-          
+            dgvNhanVien.DataSource = NhanVienController.Instance.XemNhanVien();
+            dgvNhanVien.Columns["ma"].HeaderText = @"Mã";
+            dgvNhanVien.Columns["ma"].Width = 45;
+            dgvNhanVien.Columns["ten"].HeaderText = @"Họ và tên";
+            dgvNhanVien.Columns["ten"].Width = 100;
+            dgvNhanVien.Columns["ngaysinh"].HeaderText = @"Ngày sinh";
+            dgvNhanVien.Columns["ngaysinh"].Width = 80;
+            dgvNhanVien.Columns["diachi"].HeaderText = @"Địa chỉ";
+            dgvNhanVien.Columns["diachi"].Width = 120;
+            dgvNhanVien.Columns["sodienthoai"].HeaderText = @"Số điện thoại";
+            dgvNhanVien.Columns["sodienthoai"].Width = 80;
+            dgvNhanVien.Columns["machucvu"].HeaderText = @"Mã chức vụ";
+            dgvNhanVien.Columns["machucvu"].Width = 60;
+            dgvNhanVien.Columns["taikhoan"].HeaderText = @"Tài khoản";
+            dgvNhanVien.Columns["taikhoan"].Width = 120;
+            dgvNhanVien.Columns["matkhau"].HeaderText = @"Mật khẩu";
+            dgvNhanVien.Columns["matkhau"].Width = 100;
         }
 
         private void dgvNhanVien_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            txtMa.Text = dgvNhanVien.CurrentRow.Cells[0].Value.ToString();
+            txtTen.Text = dgvNhanVien.CurrentRow.Cells[1].Value.ToString();
+            dtpNgaySinh.Text = dgvNhanVien.CurrentRow.Cells[2].Value.ToString();
+            txtDiaChi.Text = dgvNhanVien.CurrentRow.Cells[3].Value.ToString();
+            txtSoDienThoai.Text = dgvNhanVien.CurrentRow.Cells[4].Value.ToString();
+            cbMaChucVu.Text = dgvNhanVien.CurrentRow.Cells[5].Value.ToString();
+            txtTaiKhoan.Text = dgvNhanVien.CurrentRow.Cells[6].Value.ToString();
+            txtMatKhau.Text = dgvNhanVien.CurrentRow.Cells[7].Value.ToString();
         }
         #endregion
 
