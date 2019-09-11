fd6d1e11b1e769b7a7ea4acc396a815548743154
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-11 21:27:23 +0700

    Table update khohang

--- bai6quanlysieuthi/Models/khachhang.cs
+++ bai6quanlysieuthi/Models/khachhang.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bai6quanlysieuthi.Models
+﻿namespace bai6quanlysieuthi.Models
 {
-    class khachhang
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Data;
+    [Table("khachhang")]
+    public partial class khachhang
     {
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public khachhang()
+        //{
+        //    khachmuahangs = new HashSet<khachmuahang>();
+        //}
+
+        [Key]
+        [StringLength(20)]
+        public string ma { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ten { get; set; }
+
+        [StringLength(200)]
+        public string diachi { get; set; }
+
+        [StringLength(15)]
+        public string sodienthoai { get; set; }
+
+        public float uudai { get; set; }
+
+        
+
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<khachmuahang> khachmuahangs { get; set; }
     }
 }
--- bai6quanlysieuthi/Models/khohang.cs
+++ bai6quanlysieuthi/Models/khohang.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bai6quanlysieuthi.Models
+﻿namespace bai6quanlysieuthi.Models
 {
-    class khohang
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
+    using System.Data.Entity.Spatial;
+
+    [Table("khohang")]
+    public partial class khohang
     {
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public khohang()
+        //{
+        //    phieukiemkes = new HashSet<phieukiemke>();
+        //    phieunhapkhoes = new HashSet<phieunhapkho>();
+        //    phieuxuatkhoes = new HashSet<phieuxuatkho>();
+        //}
+
+        [Key]
+        [StringLength(20)]
+        public string ma { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ten { get; set; }
+
+        [StringLength(200)]
+        public string diachi { get; set; }
+
+        public khohang(string ma, string ten, string diachi)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.diachi = diachi;
+        }
+
+        
     }
 }
