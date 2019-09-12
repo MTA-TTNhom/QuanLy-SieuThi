756725b72d112c3202b888174bd3c5e3e66167b4
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-12 21:32:52 +0700

    Sua lai bang du lieu

--- bai6quanlysieuthi/Models/phieuxuatkho.cs
+++ bai6quanlysieuthi/Models/phieuxuatkho.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bai6quanlysieuthi.Models
+﻿namespace bai6quanlysieuthi.Models
 {
-    class phieuxuatkho
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data;
+    using System.Data.Entity.Spatial;
+
+    [Table("phieuxuatkho")]
+    public partial class phieuxuatkho
     {
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        //public phieuxuatkho()
+        //{
+        //    chitietphieuxuats = new HashSet<chitietphieuxuat>();
+        //}
+
+        [Key]
+        [StringLength(20)]
+        public string ma { get; set; }
+
+        public DateTime? ngayxuat { get; set; }
+
+        public float tonggiatri { get; set; }
+
+       
+
+        public phieuxuatkho(string ma,  DateTime ngayxuat, float tonggiatri, )
+        {
+            this.ma = ma;
+           
+            this.ngayxuat = ngayxuat;
+            this.tonggiatri = tonggiatri;
+           
+        }
+
+       
+        //[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        //public virtual ICollection<chitietphieuxuat> chitietphieuxuats { get; set; }
+
+        //public virtual khohang khohang { get; set; }
+
+        //public virtual nhanvien nhanvien { get; set; }
+
+        //public virtual quayhang quayhang { get; set; }
     }
 }
--- bai6quanlysieuthi/Models/sysdiagram.cs
+++ bai6quanlysieuthi/Models/sysdiagram.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bai6quanlysieuthi.Models
+﻿namespace bai6quanlysieuthi.Models
 {
-    class sysdiagram
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class sysdiagram
     {
+        [Required]
+        [StringLength(128)]
+        public string name { get; set; }
+
+        public int principal_id { get; set; }
+
+        [Key]
+        public int diagram_id { get; set; }
+
+        public int? version { get; set; }
+
+        public byte[] definition { get; set; }
     }
 }
