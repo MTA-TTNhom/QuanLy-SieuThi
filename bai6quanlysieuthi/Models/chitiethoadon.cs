c555531be22f95192c1809d0dac887c8a281336c
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-18 15:29:23 +0700

    Huynh

--- bai6quanlysieuthi/Models/chitiethoadon.cs
+++ bai6quanlysieuthi/Models/chitiethoadon.cs
@@ -21,4 +21,27 @@ namespace bai6quanlysieuthi.Models
 
         public int soluong { get; set; }
 
-        public float dongi
+        public float dongia { get; set; }
+
+        public chitiethoadon(int stt, string mahd,string mamh,int sl,float dongia)
+        {
+            this.stt = stt;
+            this.mahoadon = mahd;
+            this.mamathang = mamh;
+            this.soluong = sl;
+            this.dongia = dongia;
+        }
+
+        public chitiethoadon(DataRow row)
+        {
+            this.stt = (int)Convert.ToInt32(row["stt"].ToString());
+            this.mahoadon = row["mahoadon"].ToString();
+            this.mamathang = row["mamathang"].ToString();
+            this.soluong = (int)Convert.ToInt32(row["soluong"].ToString());
+            try {
+                this.dongia = (float)Convert.ToDouble(row["dongia"].ToString());
+            }
+            catch { }
+        }
+    }
+}
