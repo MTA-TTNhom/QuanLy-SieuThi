b05269b6441e0288308a4b4fcb083dba1b3a7be2
HuynhLeHoang <chunickdeptrailam@gmail.com>
2019-09-20 23:38:00 +0700

    Huynh

--- bai6quanlysieuthi/Controller/CtHoaDonController.cs
+++ bai6quanlysieuthi/Controller/CtHoaDonController.cs
@@ -15,14 +15,6 @@ namespace bai6quanlysieuthi.Controller
         public static CtHoaDonController Instance
         {
             get
-            {
-                if (instance == null)
-                    instance = new CtHoaDonController();
-                return CtHoaDonController.instance;
-            }
-            private set { CtHoaDonController.instance = value; }
-        }
-
         private CtHoaDonController() { }
 
         public List<chitiethoadon> XemCtHoaDon()
@@ -40,13 +32,7 @@ namespace bai6quanlysieuthi.Controller
         // thêm chi tiết hóa đơn
         public bool InsertCTHD(string mahd,string mamh,int sl)
         {
-            //string query = string.Format("insert chitiethoadon(mahoadon,mamathang,soluong) values(N'{0}',N'{1}',N'{2}')", mahd, mamh, sl);
-            string query = string.Format("exec proc_giatien N'{0}',N'{1}',{2}", mahd, mamh, sl);
-            return DataProvider.Instance.ExecuteNonQuery(query) > 0;
-        }
-        // sửa chi tiết hóa đơn
-        public bool UpdateCTHD(int stt,string mahd,string mamh,int sl)
-        {
+            //string que
             //string query = string.Format("update chitiethoadon set mahoadon=N'{0}',mamathang=N'{1}',soluong={2} where stt={3}", mahd, mamh, sl, stt);
             string query = string.Format("exec proc_updateCtHd N'{0}',N'{1}',{2},{3}", mahd, mamh, sl, stt);
             return DataProvider.Instance.ExecuteNonQuery(query) > 0;
@@ -69,11 +55,7 @@ namespace bai6quanlysieuthi.Controller
                 list.Add(ct);
             }
             return list;
-         }
-
-        public List<chitiethoadon> SearchCtHd1(string mamathang)
-        {
-            List<chitiethoadon> list = new List<chitiethoadon>();
+         }new List<chitiethoadon>();
             string query = string.Format("select * from chitiethoadon ct inner join mathang mh on ct.mamathang=mh.ma where mh.ma like N'%{0}%' ", mamathang);
             DataTable data = DataProvider.Instance.ExecuteQuery(query);
             foreach (DataRow i in data.Rows)
