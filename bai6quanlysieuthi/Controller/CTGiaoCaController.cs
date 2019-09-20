02cf9599306064873bb2bf8eedc9e6f1955f9c1c
tronglong1998 <54692940+tronglong1998@users.noreply.github.com>
2019-09-20 10:28:25 +0700

    CTGiaoCaController

---
+++ bai6quanlysieuthi/Controller/CTGiaoCaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bai6quanlysieuthi.Bus;
+using bai6quanlysieuthi.Models;
+using System.Data;
+
+namespace bai6quanlysieuthi.Controller
+{
+    class CTGiaoCaController
+    {
+        public static CTGiaoCaController instance;
+        public static CTGiaoCaController Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new CTGiaoCaController();
+                return CTGiaoCaController.instance;
+            }
+            private set { CTGiaoCaController.instance = value; }
+        }
+
+        public CTGiaoCaController() { }
+        // xem chi tiết phiếu giao ca
+        
+    }
+}
