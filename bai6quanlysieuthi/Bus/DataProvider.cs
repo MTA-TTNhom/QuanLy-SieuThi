1558b06cab71f47a8b70e60e631bd93edd1dd76f
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-23 07:49:16 +0700

    update DataProvider.cs

---
+++ bai6quanlysieuthi/Bus/DataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai6quanlysieuthi.Bus
+{
+    public class DataProvider
+    {
+        private static DataProvider instance;
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null) instance = new DataProvider();
+                return DataProvider.instance;
+            }
+            private set
+            {
+                DataProvider.instance = value;
+            }
+        }
+        private DataProvider() { }
+
+        private string connectionSTR = "Data Source=THANH-HOANG-THU\\THANH-HOANG-THU;Initial Catalog=banhangsieuthi;Integrated Security=True";
+    }
+}
--- bai6quanlysieuthi/KhachHang.Designer.cs
+++ bai6quanlysieuthi/KhachHang.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KhachHang));
             this.mnsKhachHang = new System.Windows.Forms.MenuStrip();
             this.menuTrangChu = new System.Windows.Forms.ToolStripMenuItem();
             this.menuHangHoa = new System.Windows.Forms.ToolStripMenuItem();
@@ -116,28 +115,24 @@
             this.menuTrangChu.Name = "menuTrangChu";
             this.menuTrangChu.Size = new System.Drawing.Size(90, 25);
             this.menuTrangChu.Text = "Trang chủ";
-      
             // 
             // menuHangHoa
             // 
             this.menuHangHoa.Name = "menuHangHoa";
             this.menuHangHoa.Size = new System.Drawing.Size(89, 25);
             this.menuHangHoa.Text = "Hàng hóa";
-     
             // 
             // menuNhanVien
             // 
             this.menuNhanVien.Name = "menuNhanVien";
             this.menuNhanVien.Size = new System.Drawing.Size(93, 25);
             this.menuNhanVien.Text = "Nhân viên";
-   
             // 
             // menuHuongDan
             // 
             this.menuHuongDan.Name = "menuHuongDan";
             this.menuHuongDan.Size = new System.Drawing.Size(100, 25);
             this.menuHuongDan.Text = "Hướng dấn";
-   
             // 
             // tabControl1
             // 
@@ -202,6 +197,7 @@
             // btnSearchKh
             // 
             this.btnSearchKh.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSearchKh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnSearchKh.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearchKh.Location = new System.Drawing.Point(555, 8);
             this.btnSearchKh.Name = "btnSearchKh";
@@ -209,11 +205,11 @@
             this.btnSearchKh.TabIndex = 1;
             this.btnSearchKh.Text = "Tìm kiếm";
             this.btnSearchKh.UseVisualStyleBackColor = false;
-   
             // 
             // btnExitKH
             // 
             this.btnExitKH.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExitKH.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnExitKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExitKH.Location = new System.Drawing.Point(756, 373);
             this.btnExitKH.Name = "btnExitKH";
@@ -221,11 +217,11 @@
             this.btnExitKH.TabIndex = 7;
             this.btnExitKH.Text = "Thoát";
             this.btnExitKH.UseVisualStyleBackColor = false;
-
             // 
             // btnDeleteKh
             // 
             this.btnDeleteKh.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDeleteKh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnDeleteKh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDeleteKh.Location = new System.Drawing.Point(756, 319);
             this.btnDeleteKh.Name = "btnDeleteKh";
@@ -233,11 +229,11 @@
             this.btnDeleteKh.TabIndex = 5;
             this.btnDeleteKh.Text = "Xóa";
             this.btnDeleteKh.UseVisualStyleBackColor = false;
-      
             // 
             // btnUpdate
             // 
             this.btnUpdate.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUpdate.Location = new System.Drawing.Point(756, 265);
             this.btnUpdate.Name = "btnUpdate";
@@ -245,11 +241,11 @@
             this.btnUpdate.TabIndex = 4;
             this.btnUpdate.Text = "Sửa";
             this.btnUpdate.UseVisualStyleBackColor = false;
-      
             // 
             // btnInsertKh
             // 
             this.btnInsertKh.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInsertKh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnInsertKh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInsertKh.Location = new System.Drawing.Point(756, 214);
             this.btnInsertKh.Name = "btnInsertKh";
@@ -257,11 +253,11 @@
             this.btnInsertKh.TabIndex = 3;
             this.btnInsertKh.Text = "Thêm";
             this.btnInsertKh.UseVisualStyleBackColor = false;
- 
             // 
             // btnViewKh
             // 
             this.btnViewKh.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnViewKh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnViewKh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnViewKh.Location = new System.Drawing.Point(756, 160);
             this.btnViewKh.Name = "btnViewKh";
@@ -269,7 +265,6 @@
             this.btnViewKh.TabIndex = 2;
             this.btnViewKh.Text = "Xem";
             this.btnViewKh.UseVisualStyleBackColor = false;
-    
             // 
             // dgvKh
             // 
@@ -278,7 +273,6 @@
             this.dgvKh.Name = "dgvKh";
             this.dgvKh.Size = new System.Drawing.Size(695, 220);
             this.dgvKh.TabIndex = 1;
-       
             // 
             // groupBox1
             // 
@@ -418,7 +412,6 @@
             this.btnExitHD.TabIndex = 13;
             this.btnExitHD.Text = "Thoát";
             this.btnExitHD.UseVisualStyleBackColor = false;
-   
             // 
             // btnDeleteHD
             // 
@@ -430,7 +423,6 @@
             this.btnDeleteHD.TabIndex = 11;
             this.btnDeleteHD.Text = "Xóa";
             this.btnDeleteHD.UseVisualStyleBackColor = false;
-      
             // 
             // btnUpdateHD
             // 
@@ -442,7 +434,6 @@
             this.btnUpdateHD.TabIndex = 10;
             this.btnUpdateHD.Text = "Sửa";
             this.btnUpdateHD.UseVisualStyleBackColor = false;
- 
             // 
             // btnInsertHD
             // 
@@ -454,7 +445,6 @@
             this.btnInsertHD.TabIndex = 9;
             this.btnInsertHD.Text = "Thêm";
             this.btnInsertHD.UseVisualStyleBackColor = false;
-
             // 
             // btnViewHD
             // 
@@ -466,7 +456,6 @@
             this.btnViewHD.TabIndex = 8;
             this.btnViewHD.Text = "Xem";
             this.btnViewHD.UseVisualStyleBackColor = false;
-
             // 
             // dgvHoaDon
             // 
@@ -475,7 +464,6 @@
             this.dgvHoaDon.Name = "dgvHoaDon";
             this.dgvHoaDon.Size = new System.Drawing.Size(691, 246);
             this.dgvHoaDon.TabIndex = 2;
-
             // 
             // panel2
             // 
@@ -498,7 +486,6 @@
             this.btnChiTietHD.TabIndex = 1;
             this.btnChiTietHD.Text = "Chi tiết ";
             this.btnChiTietHD.UseVisualStyleBackColor = false;
-
             // 
             // txtSearchHD
             // 
@@ -528,7 +515,6 @@
             this.btnSearchHD.TabIndex = 0;
             this.btnSearchHD.Text = "Tìm kiếm";
             this.btnSearchHD.UseVisualStyleBackColor = false;
-
             // 
             // groupBox2
             // 
@@ -672,7 +658,7 @@
             this.ClientSize = new System.Drawing.Size(884, 511);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.mnsKhachHang);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.MainMenuStrip = this.mnsKhachHang;
             this.Name = "KhachHang";
             this.Text = "KhachHang";
