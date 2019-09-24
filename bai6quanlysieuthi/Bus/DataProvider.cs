7eb55c81c3b62d57c2e5d33c9d9259c3d678b51e
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-24 08:00:09 +0700

    update DataProvider

--- bai6quanlysieuthi/Bus/DataProvider.cs
+++ bai6quanlysieuthi/Bus/DataProvider.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace bai6quanlysieuthi.Bus
 {
@@ -24,5 +26,34 @@ namespace bai6quanlysieuthi.Bus
         private DataProvider() { }
 
         private string connectionSTR = "Data Source=THANH-HOANG-THU\\THANH-HOANG-THU;Initial Catalog=banhangsieuthi;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+
+
     }
 }
