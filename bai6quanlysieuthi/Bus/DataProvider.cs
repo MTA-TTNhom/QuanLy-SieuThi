2f7e76a776e19397df276655993ff1a82ff4b6e8
BuiThanhHoang <54828059+BuiThanhHoang@users.noreply.github.com>
2019-09-25 08:00:43 +0700

    update Provider

--- bai6quanlysieuthi/Bus/DataProvider.cs
+++ bai6quanlysieuthi/Bus/DataProvider.cs
@@ -54,6 +54,31 @@ namespace bai6quanlysieuthi.Bus
             return data;
         }
 
-
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
     }
 }
